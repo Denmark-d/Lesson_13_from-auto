b3a7c65304f126a8410e34c9f4426afc55297691
daniiaadiiakova <adiyakova-d@mail.ru>
2022-04-13 16:01:44 +0300

    added owner

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -9,16 +9,25 @@ import org.aeonbits.owner.Config;
 })
 public interface ProjectConfig extends Config {
 
+    @Key("browser")
     @DefaultValue("chrome")
     String browser();
+    @Key("browserVersion")
     @DefaultValue("99.0")
     String browserVersion();
+    @Key("browserSize")
     @DefaultValue("1920x1080")
     String browserSize();
+    @Key("browserMobileView")
     String browserMobileView();
+    @Key("remoteDriverUrl")
     String remoteDriverUrl();
+    @Key("videoStorage")
     String videoStorage();
+    @Key("login")
     String login();
+    @Key("password")
     String password();
+    @Key("url")
     String url();
 }
